6f6a36b380b07d15edbc3d329ccc2f3b34188e3d
baffisebastianoITIS <baffi.sebastiano@polotecnologicoimperiese.it>
2026-02-27 11:12:30 +0100

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,14 +1,5 @@
 function startGame() {
-    document.getElementById('question').innerText = "Il linguaggio è compilato?";
-    const btnContainer = document.querySelector('.buttons');
-    btnContainer.innerHTML = `
-        <button onclick="sendAnswer('si')">SÌ</button>
-        <button onclick="sendAnswer('no')">NO</button>
-    `;
-}
-
-function startGame() {
-    // Chiamiamo il PHP con 'reset' per pulire la sessione
+    console.log("Pulsante cliccato!"); // Questo serve per vedere se funziona
     fetch('game_logic.php?answer=reset')
         .then(response => response.json())
         .then(data => {
@@ -18,5 +9,19 @@ function startGame() {
                 <button onclick="sendAnswer('si')">SÌ</button>
                 <button onclick="sendAnswer('no')">NO</button>
             `;
+        })
+        .catch(err => console.error("Errore nel caricamento:", err));
+}
+
+function sendAnswer(val) {
+    fetch('game_logic.php?answer=' + val)
+        .then(response => response.json())
+        .then(data => {
+            if (data.type === 'question') {
+                document.getElementById('question').innerText = data.text;
+            } else if (data.type === 'result') {
+                document.getElementById('question').innerHTML = "Ho indovinato! È <strong>" + data.text + "</strong>!";
+                document.querySelector('.buttons').innerHTML = '<button onclick="location.reload()">Gioca ancora</button>';
+            }
         });
 }
